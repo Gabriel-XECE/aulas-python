65b223422ff22fb9311e421d5eba3ee5e379b859
Gabriel-XECE <gabriel.alvex@gmail.com>
2024-09-10 11:42:14 -0300

    Add files via upload

---
+++ Aula 01/atv1.py
@@ -0,0 +1,8 @@
+
+
+
+
+str =  input ("digite seu nome:")
+int = input ("digite sua idade:")
+float = input ("digite sua altura:")
+print (input)
---
+++ Aula 01/atv3.py
@@ -0,0 +1,16 @@
+num1 = 62
+num2 = 11
+
+print("adição:", num1+ num2)
+print("Subtração", num1 - num2)
+print("Multiplicação:", num1 * num2)
+print("dvisão:", num1 / num2)
+print("Divisão interia", num1 // num2)
+print("Módulo:", num1 % num2)
+print("Exponenciação:", num1** num2)
+
+
+nome = "Inifinite School"
+saudacão = (f"Olá, eu sou {nome} e dou as boas-vindas a todos vocês!")
+
+print(saudacão)
